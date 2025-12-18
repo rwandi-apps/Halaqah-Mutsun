cd62f63df8a2ae0f0408a36e8346d6db306c210b
rwandi-apps <rwandi1989@gmail.com>
2025-12-18 22:31:07 +0700

    Update page.tsx

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,8 +1,6 @@
-
 import React, { useState } from 'react';
-// Fix: Use default imports instead of named imports for LogoSDQ and Button
-import LogoSDQ from '../../../components/LogoSDQ';
-import Button from '../../../components/Button';
+import { LogoSDQ } from '../../../components/LogoSDQ';
+import { Button } from '../../../components/Button';
 import { login, mockLogin } from '../../../lib/auth';
 import { User, Role } from '../../../types';
 import { isFirebaseEnabled } from '../../../lib/firebase';
