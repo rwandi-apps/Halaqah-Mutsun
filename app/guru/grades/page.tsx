29d7f1083673294659f289699489bb3da065d49a
rwandi-apps <rwandi1989@gmail.com>
2025-12-19 10:33:31 +0700

    Create page.tsx

---
+++ app/guru/grades/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import ComingSoonPage from "@/components/ComingSoonPage";
+
+export default function GradesPage() {
+  return (
+    <ComingSoonPage
+      title="Input Nilai Rapor"
+      description="Fitur input nilai rapor siswa sedang disiapkan agar sesuai dengan standar kurikulum SDQ."
+    />
+  );
+}
