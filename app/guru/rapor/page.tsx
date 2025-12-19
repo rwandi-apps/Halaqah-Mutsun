4d6944331a446c75e281e012e2bb235b423ab44a
rwandi-apps <rwandi1989@gmail.com>
2025-12-19 10:08:12 +0700

    Create page.tsx

---
+++ app/guru/rapor/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import ComingSoon from "@/components/ComingSoon";
+
+export default function RaporPage() {
+  return (
+    <ComingSoon
+      title="Rapor Siswa"
+      description="Fitur rapor digital dan rekap capaian siswa akan segera hadir."
+    />
+  );
+}
