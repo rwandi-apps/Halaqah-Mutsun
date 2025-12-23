57d24d71b818d5e9db93b685cbaaf77545ba5872
rwandi-apps <rwandi1989@gmail.com>
2025-12-23 21:57:41 +0700

    Update calculateTahfizhRange.ts

--- calculateTahfizhRange.ts
+++ calculateTahfizhRange.ts
@@ -3,6 +3,7 @@ import { AYAT_DB, AyatEntry } from "./ayatDB";
 export interface TahfizhResult {
   totalLines: number;
   totalPages: number;
+  isPartial: boolean; // penanda data belum lengkap
 }
 
 export function calculateTahfizhRange(
@@ -14,6 +15,7 @@ export function calculateTahfizhRange(
 
   let isInRange = false;
   const selected: AyatEntry[] = [];
+  let reachedEnd = false;
 
   for (const ayat of AYAT_DB) {
 
@@ -26,12 +28,14 @@ export function calculateTahfizhRange(
     }
 
     if (ayat.surah === endSurah && ayat.ayat === endAyat) {
+      reachedEnd = true;
       break;
     }
   }
 
   return {
     totalLines: selected.reduce((s, a) => s + a.lines, 0),
-    totalPages: new Set(selected.map(a => a.page)).size
+    totalPages: new Set(selected.map(a => a.page)).size,
+    isPartial: !reachedEnd
   };
 }
